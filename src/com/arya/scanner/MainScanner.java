8ec037d08e1adb7707bcb584624abf767b00a2d4
Ribham <128628893+Ribham@users.noreply.github.com>
2024-03-01 20:46:16 +0530

    Delete src/com/arya directory

--- src/com/arya/scanner/MainScanner.java
+++
@@ -1,9 +0,0 @@
-package com.arya.scanner;
-import com.arya.scanner.utility.ScannerUtility;
-public class MainScanner {
-    public static void main(String[] args) {
-        ScannerUtility utility = new ScannerUtility();
-//		utility.acceptNumbers();
-        utility.acceptCharacters();
-    }
-}
